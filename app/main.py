a37e009c39a004d73c4e291959a0f65fafe4eebb
arihant1805 <22cs3016@rgipt.ac.in>
2024-06-10 17:23:31 +0530

    Pre-final phase

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 import tensorflow as tf
-from fastapi import FastAPI, 
+from fastapi import FastAPI, File
 from tensorflow.keras.models import load_model
+import cv2 as cv
+import numpy as np
 
 model = load_model('model.h5')
 
 app = FastAPI()
 
 @app.post("/")
-def check(file :)
+def check(file : File):
+    img = cv.resize(file, (256, 256))
+    img = np.array([img])
+    pred = model.predict(img)[0]
 
+    if pred >= 0.5:
+        return {"You have corona."}
+    
+    else:
+        return {"You are healthy"}
