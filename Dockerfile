22d5e80bbbf5f504a470682bd0765e616b7db4ac
arihant1805 <22cs3016@rgipt.ac.in>
2024-08-04 17:22:21 +0530

    Final commit

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.11.9-slim
-COPY . /app
-WORKDIR /app/app
+COPY ./app .
+RUN pip install upgrade-pip
 RUN pip install -r requirements.txt 
-CMD ["uvicorn", "main:app"]
+EXPOSE 8000
+CMD ["uvicorn", "main:app","--port","8000","--host","0.0.0.0"]
--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI , UploadFile
 import  tensorflow as tf
-import cv2 as cv
 import numpy as np
+from PIL import Image
+import io
 
 app = FastAPI()
 
@@ -14,11 +15,10 @@ def home():
 @app.post("/predict")
 async def check(file:UploadFile):
     contents = await file.read()
-    np_img = np.fromstring(contents, np.uint8)
-    image = cv.imdecode(np_img, cv.IMREAD_COLOR)
-    file_resized = cv.resize(image, (256, 256))
-    file = np.array([file_resized])
-    pred=model.predict(file)
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
+    image_resized = image.resize((256, 256))
+    file = np.array([image_resized])
+    pred=model.predict(file, verbose=0)
     if pred[0] >= 0.5:
         return {"msg": "COVID-19 found. Consult a docter"}
     else :
