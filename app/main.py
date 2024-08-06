eb9c754de358616038cc61354b36e4f7cd4461b3
arihant1805 <22cs3016@rgipt.ac.in>
2024-08-06 19:55:19 +0530

    Rectified model

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ import  tensorflow as tf
 import numpy as np
 from PIL import Image
 import io
-
 app = FastAPI()
 
 model = tf.keras.models.load_model('model.h5')
@@ -17,9 +16,16 @@ async def check(file:UploadFile):
     contents = await file.read()
     image = Image.open(io.BytesIO(contents)).convert("RGB")
     image_resized = image.resize((256, 256))
-    file = np.array([image_resized])
+    file = np.array([image_resized])/255
     pred=model.predict(file, verbose=0)
-    if pred[0] >= 0.5:
-        return {"msg": "COVID-19 found. Consult a docter"}
+    print(pred.shape)
+    if pred[0,0] >= 0.5:
+        return {
+            "msg": "COVID-19 found. Consult a docter",
+            "code":1
+            }
     else :
-        return {"msg": "COVID-19 not found. You are fine!!"}
+        return {
+            "msg": "COVID-19 not found. You are fine!!",
+            "code":0
+                }
